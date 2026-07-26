9755b226e64e43036217c2ba821ebc297076051e
shashikant347 <yshashikant231217@gmail.com>
2026-07-26 14:50:30 +0530

    fixing the route

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ const appRouter  = require('./appRouter')
 const connectDB = require('./config/connectDB')
 const cookie = require('cookie-parser');
 const cookieParser = require('cookie-parser');
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 
 dotenv.config();
--- backend/src/router/request.js
+++ backend/src/router/request.js
@@ -2,11 +2,13 @@
 const express = require('express')
 const requestController = require('../controller/request')
 const { userAuth }= require('../middleware/auth')
+const { model } = require('mongoose')
 
 
 const requestRouter = express.Router()
 
 
-requestRouter.get('/request/:status/:id',userAuth,requestController.sendConnectionRequest)
+requestRouter.get('/:status/:id',userAuth,requestController.sendConnectionRequest)
 
 
+module.exports = requestRouter;
